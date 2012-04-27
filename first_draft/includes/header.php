2110492419b2a6439e1cc5f3d8f0d46c049283d8
Nicole Cooprider <nmc58@cornell.edu>
2012-04-26 22:25:19 -0400

    Here is the first draft of the skeleton I have created for our project. It is written in PHP, so if you need to comment certain things out, feel free to do that on your local computer.

---
+++ first_draft/includes/header.php
@@ -0,0 +1,5 @@
+<?php
+
+print("<p>header</p>");
+
+?>
---
+++ first_draft/place_an_order.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Country Cupcakes</title>
+    <link rel="stylesheet" href="style/style.css" type="text/css"/>
+</head>
+<body>
+    <?php
+    include("includes/header.php");
+    include("includes/navbar.php");
+
+    ?>
+    <p>VERY COOL ORDER FORM</p>
+    
+    <?php
+    include("includes/footer.php");
+    ?>
+</body>
+</html>
