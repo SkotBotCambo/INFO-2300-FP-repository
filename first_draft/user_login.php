785d3f0f8fa7c489022ea6d64782587df34562dd
Nicole Cooprider <nmc58@cornell.edu>
2012-05-02 15:34:13 -0400

    login page updates

--- first_draft/user_login.php
+++ first_draft/user_login.php
@@ -12,11 +12,60 @@ session_start();
     <?php
     include("includes/header.php");
     include("includes/navbar.php");
-
+    include("includes/passwords.php");
     ?>
-    <p>Here is where users login.</p>
-    
+    <div id="login">
+        <h1>Log in</h1>
+        <form action="" method="">
+            <p>Username:<input type="text" name="username" value=""/><br/>
+            Password:<input type="password" name="password" value=""><br/>
+            <input type="submit" value="Sign in" name="submit"/></p>
+        </form>
+    </div>
+    <div id="signup">
+        <h1>New User?</h1>
+        <form action="" method="">
+            <table>
+                <tr>
+                    <td>Username:</td><td><input type="text" name="newusername" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Password:</td><td><input type="password" name="newpassword" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Name:</td><td><input type="text" name="u_name" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Address:</td><td><input type="text" name="address" value=""/></td>
+                </tr>
+                <tr>
+                    <td>City:</td><td><input type="text" name="city" value=""/></td>
+                </tr>
+                <tr>
+                    <td>State:</td><td><input type="text" name="state" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Zip Code:</td><td><input type="text" name="zipcode" value=""/></td>
+                </tr>
+            </table>
+            <input type="submit" value="Make New Account" name="newsubmit"/></p>
+        </form>
+    </div>
     <?php
+    if(isset($_POST['username'])&&isset($_POST['password'])&&!isset($_SESSION['logged_user'])) {
+        $query = "SELECT * FROM Users WHERE username = ? AND password = ?";
+        $stmt = $mysqli->stmt_init();
+        if ($stmt->prepare($query)) {
+            $stmt->bind_param('ss', $_POST['username'], "".hash('sha256', $_POST['password'].$gibberish));
+            $result=$stmt->execute();
+        }
+        while ($row = $stmt->fetch_row()){
+            print($row);
+        }
+        else print("It got here.");
+    }
+    
+    
     include("includes/footer.php");
     ?>
 </body>
