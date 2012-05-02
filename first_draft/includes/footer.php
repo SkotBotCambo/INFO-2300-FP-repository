3beb37ce0aeeb169e112e8c7798ca0b6a8809358
Scott Cambo <sac355@cornell.edu>
2012-05-02 00:22:41 -0400

    Scott did a lot of styling (still not enough) to the home page, added a header logo, added filler photo for home page, positioning on main photo, main content, footer added home page content

--- first_draft/includes/footer.php
+++ first_draft/includes/footer.php
@@ -2,7 +2,7 @@
 
 print("
 	<div id=\"footer\">
-		<p>footer</p>
+		<p>&copy; 2012 Cupcake Country<br/> Site Designed by Holly Domke, Nicole Cooprider, and Scott Cambo</p>
 	</div>
 </div>\n"
 );
--- first_draft/includes/navbar.php
+++ first_draft/includes/navbar.php
@@ -1,10 +1,23 @@
 
 	<div id="menu">
 		<ul>
-			<li><a href="index.php">Home</a></li>
+			<li class="nav"><a href="index.php">Home</a></li>
 			<li><a href="cupcakes.php">Cupcakes</a></li>
 			<li><a href="about_us.php">About Us</a></li>
 			<li><a href="place_an_order.php">Place an Order</a></li>
 			<li><a href="user_login.php">User Login</a></li>
 		</ul>
 	</div>
+	
+	<!-- JQuery for button fadeIn 
+	<script type="text/javascript">
+	$(".nav a").hover(
+		function(){
+		$(.nav).stop().animate({"opacity": "0"}, "slow");
+		}
+		function(){
+		$(.nav).stop().animate({
+		});
+	</script>
+	end JQuery -->
+	
