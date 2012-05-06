832590abc3b8817013018f9e79219c973d5997c2
Scott Cambo <sac355@cornell.edu>
2012-05-06 16:44:11 -0400

    testing nicole's recente merge

--- first_draft/includes/navbar.php
+++ first_draft/includes/navbar.php
@@ -2,23 +2,23 @@
 	<div id="menu">
 		<div class="home">
 			<img src="graphics/halo.png" width="180" height="60" alt="Home" />
-			<p>Home</p>
+			<p><a href="index.php">Home</a></p>
 		</div>
 		<div class="cupcakes"> 
 			<img src="graphics/halo.png" width="180" height="60" alt="cupcakes" />
-			<p>Cupcakes</p>
+			<p><a href="cupcakes.php">Cupcakes</a></p>
 		</div>
 		<div class="About_Us">
 			<img src="graphics/halo.png" width="180" height="60" alt="About Us" />
-			<p>About Us</p>
+			<p><a href="about_us.php">About Us</a></p>
 		</div>
 		<div class="Place_An_Order">
 			<img src="graphics/halo.png" width="180" height="60" alt="Place An Order" />
-			<p>Place An Order</p>
+			<p><a href="place_an_order.php">Place An Order</a></p>
 		</div>
 		<div class="User_Login">
 			<img src="graphics/halo.png" width="180" height="60" alt="User Login" />
-			<p>User Login</p>
+			<p><a href="user_login.php">User Login</a></p>
 		</div>
 	</div>
 	
